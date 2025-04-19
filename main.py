2907662c9595776dac3a050cca161fcca6c34ac1
pokemogukunnsann <konnnitiha.sayounara@icloud.com>
2025-04-19 12:52:27 +0900

    Update main.py
    Signed-off-by: pokemogukunnsann <konnnitiha.sayounara@icloud.com>

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def run():
-    cmd = request.args.get("lang")
-    if not cmd:
+    long = request.args.get("lang")
+    if not long:
         return "<h1>404 Not Found</h1> <br><h2>このサイトはpokemogukunnsのコマンドラインを再現するためのサイトです．<br>まだ完成はしておらず、コマンドは実行できません．</h2><h1>せめて完成を待ってください．</h1>", 200
 
     
 
 @app.route("/run")
-def run():
+def run_command():
     cmd = request.args.get("cmd")
     if not cmd:
         return "Error: No command provided.", 400
